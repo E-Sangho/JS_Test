bd926854137f3c5ade274d3e2630c04ec5b5ff2a
Sangho <istrangeho@gmail.com>
2021-07-12 16:10:31 +0900

    event

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,10 +1,20 @@
-const player = {
-    name: "Lee",
-    age: function(a){
-        console.log(a);
-    },
-};
+const loginForm = document.querySelector("#login-form");
+const loginInput = loginForm.querySelector("input");
 
-const a = parseInt(prompt("How old are you?"));
-console.log(typeof a);
-player.age(a);
+const link = document.querySelector("a");
+
+function onLoginSubmit(event)
+{
+    event.preventDefault();
+    const username = loginInput.value;
+    console.log(username);
+}
+
+function handleLinkClick(event)
+{    
+    event.preventDefault();
+    console.dir(event);
+}
+
+loginForm.addEventListener("submit", onLoginSubmit);
+link.addEventListener("click", handleLinkClick)
